f59ada67ac86450be9eee79070d25d34b7ab9ca5
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-06-07 08:17:54 +0700

    Add tag to common attributes

--- lib/Algorithm/Backoff.pm
+++ lib/Algorithm/Backoff.pm
@@ -16,6 +16,7 @@ our %attr_consider_actual_delay = (
         summary => 'Whether to consider actual delay',
         schema => ['bool*'],
         default => 0,
+        tags => ['common'],
         description => <<'_',
 
 If set to true, will take into account the actual delay (timestamp difference).
@@ -35,6 +36,7 @@ our %attr_max_attempts = (
         summary => 'Maximum number consecutive failures before giving up',
         schema => 'uint*',
         default => 0,
+        tags => ['common'],
         description => <<'_',
 
 0 means to retry endlessly without ever giving up. 1 means to give up after a
@@ -51,6 +53,7 @@ our %attr_jitter_factor = (
     jitter_factor => {
         summary => 'How much to add randomness',
         schema => ['float*', between=>[0, 0.5]],
+        tags => ['common'],
         description => <<'_',
 
 If you set this to a value larger than 0, the actual delay will be between a
@@ -67,6 +70,7 @@ our %attr_delay_on_success = (
         summary => 'Number of seconds to wait after a success',
         schema => 'ufloat*',
         default => 0,
+        tags => ['common'],
     },
 );
 
@@ -74,7 +78,8 @@ our %attr_max_delay = (
     max_delay => {
         summary => 'Maximum delay time, in seconds',
         schema => 'ufloat*',
-    },
+         tags => ['common'],
+   },
 );
 
 $SPEC{new} = {
